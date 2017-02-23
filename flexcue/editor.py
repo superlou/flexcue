3ae9077cd350d1a348aa89e93e52c50c1e2c68a3
superlou <lousimons@gmail.com>
2017-02-22 20:48:27 -0500

    Made the text control do some editing

--- flexcue/editor.py
+++ flexcue/editor.py
@@ -6,14 +6,23 @@ from .prompter_monitor import PrompterMonitor
 
 class Editor(wx.Frame):
     def __init__(self, parent, title):
-        super().__init__(parent, title=title, size=(300, 200))
+        super().__init__(parent, title=title)
         self.prompter = None
+        self.script = """[SHAMAN]
+You’re lucky to be alive. Many
+strong men have fallen to the
+gatekeepers.
+
+[SHAMAN]
+Here, take a sip."""
+
         self.init_ui()
         self.Show()
 
     def create_prompter(self):
         self.prompter = Prompter(None, title='Prompter')
         self.monitor.prompter = self.prompter
+        self.update_prompter_script()
 
     def init_ui(self):
         menubar = wx.MenuBar()
@@ -26,11 +35,24 @@ class Editor(wx.Frame):
         menubar.Append(helpm, '&Help')
         self.SetMenuBar(menubar)
 
-        self.text_ctrl = RichTextCtrl(self, size=(250, 100))
-        self.monitor = PrompterMonitor(self, size=(100, 100))
+        self.rtc = RichTextCtrl(self, size=(250, 240))
+        self.rtc.Bind(wx.EVT_KEY_UP, self.key_up)
+        self.load_script(self.rtc, self.script)
+        self.monitor = PrompterMonitor(self, size=(320, 240))
 
         hbox = wx.BoxSizer(wx.HORIZONTAL)
-        hbox.Add(self.text_ctrl)
+        hbox.Add(self.rtc)
         hbox.Add(self.monitor)
         self.SetAutoLayout(True)
         self.SetSizer(hbox)
+
+    def key_up(self, event):
+        self.script = self.rtc.GetValue()
+        self.update_prompter_script()
+
+    def load_script(self, rtc, script):
+        rtc.WriteText(script)
+
+    def update_prompter_script(self):
+        if self.prompter:
+            self.prompter.script = self.script
--- flexcue/prompter.py
+++ flexcue/prompter.py
@@ -3,16 +3,26 @@ import wx
 
 class Prompter(wx.Frame):
     def __init__(self, parent, title):
-        super().__init__(parent, title=title, size=(300, 300))
+        super().__init__(parent, title=title, size=(320, 240))
         self.Bind(wx.EVT_PAINT, self.paint)
         self.Show()
+        self._script = ""
+        self.font = wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+            wx.FONTWEIGHT_BOLD, False, 'Freesans 10 Pitch')
+
+    @property
+    def script(self):
+        return self._script
+
+    @script.setter
+    def script(self, new_script):
+        self._script = new_script
+        self.Refresh()
 
     def paint(self, event):
-        self.draw_line()
-
-    def draw_line(self):
         dc = wx.ClientDC(self)
-        dc.DrawLine(50, 60, 190, 60)
+        dc.SetFont(self.font)
+        dc.DrawText(self.script, 0, 0)
 
     def get_bitmap(self):
         dc = wx.ClientDC(self)
